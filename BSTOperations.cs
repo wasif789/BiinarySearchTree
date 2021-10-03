8beaa5fd4b5f4b5061830a05a53e92039a3adcce
wasif789 <wsffarid1@gmail.com>
2021-10-04 01:36:48 +0530

    create BST

---
+++ BSTOperations.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    //Generic class
+    public class BSTOperations<T> where T : IComparable
+    {
+        int count = 0;
+        public T data;
+        public BSTOperations<T> left = null, right = null;
+
+        public BSTOperations(T data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+
+        //Insert Node in BST
+        public void InsertData(T data)
+        {
+
+            T current = this.data;
+            if (current.CompareTo(data) > 0)
+            {
+                count++;
+                if (this.left == null)
+                {
+
+                    this.left = new BSTOperations<T>(data);
+                    Console.WriteLine("Node-" + count + " " + this.left.data + "->" + this.data + "  (Left Insertion)");
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Node-" + count + " " + this.left.data + "->" + this.data + " Go to Left");
+
+                    this.left.InsertData(data);
+                }
+            }
+            else
+            {
+                count++;
+                if (this.right == null)
+                {
+
+                    this.right = new BSTOperations<T>(data);
+                    Console.WriteLine("Node-" + count + " " + this.data + "<-" + this.right.data + "  (Right Insertion)");
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Node-" + count + " " + this.data + "<-" + this.right.data + " Go to Right");
+
+                    this.right.InsertData(data);
+                }
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Searh Tree Program!\n");
+            BSTOperations<int> obj = new BSTOperations<int>(56);
+            obj.InsertData(30);
+            obj.InsertData(70);
+        }
+    }
+}
